3c2f56a69f9b52497e7fc2d331a412ea01019ff5
Eric Osei Adu <eoadu013@st.ug.edu.gh>
2024-06-27 00:13:01 +0000

    Added full stop to my name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Eric Osei Adu</Text>
+      <Text>My name is Eric Osei Adu.</Text>
       <StatusBar style="auto" />
     </View>
   );
